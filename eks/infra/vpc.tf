98f09941488858ebcad09140a004cf2cc12308e8
dlsrks1218 <dlsrks1218@daum.net>
2024-01-02 15:24:02 +0900

    vpc module에 tagging 수정

--- eks/infra/vpc.tf
+++ eks/infra/vpc.tf
@@ -131,10 +131,10 @@
 # # }
 
 module "vpc" {
-  source = "../modules/vpc"
+  source = "../../modules/vpc"
 
   prefix              = "myeks-vpc"
-  vpc_cidr            = "10.0.0.0/16"
+  cidr_block            = "10.0.0.0/16"
   env                 = "test"
   public_subnet_cidr  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
